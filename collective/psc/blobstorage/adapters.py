cf3da1722bdad516cbc8965bcd24359abaf4014f
jbb666 <jbb666@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-31 17:41:41 +0000

    added missing adapter, fixed field initialization issue

--- collective/psc/blobstorage/adapters.py
+++ collective/psc/blobstorage/adapters.py
@@ -10,6 +10,26 @@ from Products.CMFDefault.interfaces import IFile
 
 from plone.app.blob.interfaces import IBlobbable
 
+import ZPublisher.HTTPRequest
+
+class BlobbableFileUpload(object):
+    """adapts for ZPublisher FileUpload to work with blobs"""
+    implements(IBlobbable)
+    adapts(ZPublisher.HTTPRequest.FileUpload)
+    
+    def __init__(self, context):
+        self.context = context
+        
+    def mimetype(self):
+        return self.context.headers['mime-type']
+        
+    def filename(self):
+        return self.context.filename
+
+    def feed(self, blob):
+        """ see interface ... """
+        blob.open('w').write(self.context.read())
+                
 class BlobbableFile(object):
     """ adapter for FileUpload objects to work with blobs """
     implements(IBlobbable)
@@ -23,6 +43,9 @@ class BlobbableFile(object):
         return self.context.content_type
 
     def filename(self):
+        # HACK! This only seems to get triggered during initial creation from createObject
+        if self._name == 'downloadableFile': 
+            return ''
         return self._name
 
     def feed(self, blob):
